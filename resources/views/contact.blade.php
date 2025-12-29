17e48e21eb27241f1a6ebef2f0511b43e87478b1
mrc-alif007-bd <mrc.alif007@gmail.com>
2025-12-29 13:13:37 +0600

    laravel 01

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends ('layouts.default')
+
+@section('content')
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat temporibus ipsa qui laboriosam, dolorum eos. Voluptatibus modi atque laboriosam voluptas omnis? Amet pariatur deserunt praesentium tempora, vel quidem beatae consequuntur error ea dignissimos earum cupiditate, similique officiis minima cum vitae. Atque vel a debitis, repudiandae consequatur fugiat error harum libero?</p>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends ('layouts.default')
+
+@section("content")
+
+<div class="jumbotron">
+  <div class="container text-center">
+    <h1>My Portfolio</h1>      
+    <p>Some text that represents "Me"...</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat temporibus ipsa qui laboriosam, dolorum eos. Voluptatibus modi atque laboriosam voluptas omnis? Amet pariatur deserunt praesentium tempora, vel quidem beatae consequuntur error ea dignissimos earum cupiditate, similique officiis minima cum vitae. Atque vel a debitis, repudiandae consequatur fugiat error harum libero?</p>
+  </div>
+</div>
+  
+
+
+@endsection
